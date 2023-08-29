24a7b18ff438443270e1707b7240acaf2fb1bb20
Nauclhlt <111179210+Nauclhlt@users.noreply.github.com>
2023-08-29 20:54:01 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+function onClickKnock()
+{
+    let audio = document.getElementById("audioKnock");
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function onClickFrog()
+{
+    let audio = document.getElementById("audioFrog");
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function onClickDoor()
+{
+    let audio = document.getElementById("audioDoor");
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function onClickRain()
+{
+    let audio = document.getElementById("audioRain");
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function onClickRainStop()
+{
+    let audio = document.getElementById("audioRain");
+    audio.pause();
+    audio.currentTime = 0;
+}
+
+function onClickThunder()
+{
+    let audio = document.getElementById("audioThunder");
+    audio.volume = 1;
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function onClickApplause()
+{
+    let audio = document.getElementById("audioApplause");
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function onClickDrumroll()
+{
+    let audio = document.getElementById("audioDrumroll");
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function onClickDrumend()
+{
+    let roll = document.getElementById("audioDrumroll");
+    roll.pause();
+    roll.currentTime = 0;
+    let audio = document.getElementById("audioDrumend");
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function onClickSpotlight()
+{
+    let audio = document.getElementById("audioSpotlight");
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function onClickBirds()
+{
+    let audio = document.getElementById("audioBirds");
+    audio.currentTime = 0;
+    audio.play();
+}
+
+function onClickBirdsStop()
+{
+    let audio = document.getElementById("audioBirds");
+    audio.pause();
+    audio.currentTime = 0;
+}
